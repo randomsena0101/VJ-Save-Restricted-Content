f9a95fbe3724477367ce5a156d461ef413d4a257
randomsena0101 <166689915+randomsena0101@users.noreply.github.com>
2026-02-26 11:19:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) # True or False
 
 if LOGIN_SYSTEM == False:
     # if login system is False then fill your tg account session below 
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "AQIzImMAI_Bzq8pvW5EtPvSvTfAfd4o7AFQd4CxBLf_KqDZMlLbcmHMPreQBjSh32nW6GctDwJcOsLBIYzgPw0P9RjOUv0wGUoOqfqxHASEV3sH_9xpez-J-TaDisZc8ILUwCXhfHOmES13sVPJA-D_rOkRolgZB5t0v-gmlD-54FfDqqYjHZZGy4plUGxVyEZrg2y3xhjWbAqTdQhvXKCjHUZ6Sy_MxmCtJ2SYeNmdjP3wpvgiU43Fog7AJ_dSGVWcjtaTs9eR-5G_R7N8WqoNxziamBEdgkxrZyizFt0tMZU3PJiMSgi8x3u1tEyX45lApMNcfitXorxGZtrOY0L5bsIKM-wAAAAHpC8m5AA")
 else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8737801641:AAE7QoFn2deJt2WfTjVDt10hJGQow0wwgHA")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "36905571"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "36677bbab05f148b95f91b13dbc57ea1")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "8204831161"))
